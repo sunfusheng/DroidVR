8688d36a1d312f9b231ccc340f6c6d8c19cf8292
sfsheng0322 <sfsheng0322@gmail.com>
2017-08-28 17:45:53 +0800

    add resources

--- app/src/main/java/com/sunfusheng/vr/MainActivity.java
+++ app/src/main/java/com/sunfusheng/vr/MainActivity.java
@@ -2,10 +2,12 @@ package com.sunfusheng.vr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ImageView;
 
 import com.sunfusheng.vr.adapter.PanoramaImageAdapter;
+import com.sunfusheng.vr.model.ModelUtil;
 import com.sunfusheng.vr.utils.ImageUtil;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,11 +30,27 @@ public class MainActivity extends AppCompatActivity {
         ivMine = (ImageView) findViewById(R.id.iv_mine);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         ImageUtil.colorImageViewDrawable(ivMine, R.color.transparent60_white);
+
+        mAdapter = new PanoramaImageAdapter(this, ModelUtil.getPanoramaImageList());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(mAdapter);
     }
 
     private void initListener() {
         ivMine.setOnClickListener(v -> {
 
+        });
+
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
         });
     }
 }
--- app/src/main/java/com/sunfusheng/vr/adapter/PanoramaImageAdapter.java
+++ app/src/main/java/com/sunfusheng/vr/adapter/PanoramaImageAdapter.java
@@ -1,9 +1,17 @@
 package com.sunfusheng.vr.adapter;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.Toast;
+
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.google.vr.sdk.widgets.pano.VrPanoramaView;
+import com.sunfusheng.vr.R;
 import com.sunfusheng.vr.model.PanoramaImageModel;
 
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -11,13 +19,48 @@ import java.util.List;
  */
 public class PanoramaImageAdapter extends BaseMultiItemQuickAdapter<PanoramaImageModel, BaseViewHolder> {
 
-    public PanoramaImageAdapter(List<PanoramaImageModel> data) {
-        super(data);
+    private Context context;
+    public VrPanoramaView vrPanoramaView;
 
+    public PanoramaImageAdapter(Context context, List<PanoramaImageModel> data) {
+        super(data);
+        this.context = context;
+        addItemType(0, R.layout.item_panorana_image);
+    }
+
+    @Override
+    public void onBindViewHolder(BaseViewHolder holder, int position) {
+        super.onBindViewHolder(holder, position);
     }
 
     @Override
     protected void convert(BaseViewHolder helper, PanoramaImageModel item) {
+        helper.setText(R.id.tv_title, item.title);
+        helper.setText(R.id.tv_desc, item.desc);
+        loadPanoramaImage(helper.getView(R.id.vrPanoramaView), item.resource);
+        helper.itemView.setOnClickListener(v -> {
+            vrPanoramaView = helper.getView(R.id.vrPanoramaView);
+            vrPanoramaView.setDisplayMode(2);
+        });
+    }
 
+    private void loadPanoramaImage(VrPanoramaView vrPanoramaView, String imageName) {
+        try {
+            InputStream inputStream = context.getAssets().open(imageName);
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            vrPanoramaView.setTouchTrackingEnabled(true);
+            vrPanoramaView.setFullscreenButtonEnabled(true);
+            vrPanoramaView.setInfoButtonEnabled(false);
+            vrPanoramaView.setStereoModeButtonEnabled(false);
+            VrPanoramaView.Options options = new VrPanoramaView.Options();
+            options.inputType = VrPanoramaView.Options.TYPE_MONO;
+            vrPanoramaView.loadImageFromBitmap(bitmap, options);
+            vrPanoramaView.setOnClickListener(v -> {
+                Toast.makeText(context, "setDisplayMode()", Toast.LENGTH_SHORT).show();
+                vrPanoramaView.setDisplayMode(2);
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
